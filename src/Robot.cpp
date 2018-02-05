d1952aaadf7356a66e0a9e0dafc803118cf1d580
Henry Fiantaca <hfiantaca@gmail.com>
2018-02-05 17:08:26 -0500

    Adding Autonomus option parsing

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -113,18 +113,28 @@ public:
 		arms->GetFaults(_faults_A);
 
 		if (_faults_L.SensorOutOfPhase) {
-			std::cout << " Left drive sensor is out of phase";
+			std::cout << " Left drive sensor is out of phase\n";
 		}
 		if (_faults_R.SensorOutOfPhase) {
-			std::cout << " Right drive sensor is out of phase";
+			std::cout << " Right drive sensor is out of phase\n";
 		}
 		if (_faults_A.SensorOutOfPhase) {
-			std::cout << " Arm sensor is out of phase";
+			std::cout << " Arm sensor is out of phase\n";
 		}
 	}
 
 	void AutonomousInit() {
-		
+		// Collect Options
+		std::string targets = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+		char optionTarget = targets[0];
+		std::string optionStart = startingPosition.GetSelected();
+		std::string optionDrop = autoDrop.GetSelected();
+		std::string optionDelay = autoDelay.GetSelected();
+		// Output to console, for debugging
+		std::cout << "Switch target is " << optionTarget << " from " << targets << "\n";
+		std::cout << "Starting from " << optionStart << "\n";
+		std::cout << "Power Cube Drop? " << optionDrop << "\n";
+		std::cout << "Delay Option: " << optionDelay << "\n";
 	}
 	
 	

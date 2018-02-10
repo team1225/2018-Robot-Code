ab18ff2675333d0d745e3d25ad4261c8d74a51bf
Henry Fiantaca <hfiantaca@gmail.com>
2018-02-10 11:26:44 -0500

    Add inital PID settings to Auto (not tested)

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -116,6 +116,18 @@ public:
 		// Disabling Safety
 		robotDrive->SetSafetyEnabled(false);
 
+		// Setting PID paramaters
+		leftDrive->selectProfileSlot(0, 0);
+		leftDrive->Config_kF(0, 0.2, Constants.kTimeoutMs);
+		leftDrive->Config_kP(0, 0.2, Constants.kTimeoutMs);
+		leftDrive->Config_kI(0, 0, Constants.kTimeoutMs);
+		leftDrive->Config_kD(0, 0, Constants.kTimeoutMs);
+		rightDrive->selectProfileSlot(0, 0);
+		rightDrive->Config_kF(0, 0.2, Constants.kTimeoutMs);
+		rightDrive->Config_kP(0, 0.2, Constants.kTimeoutMs);
+		rightDrive->Config_kI(0, 0, Constants.kTimeoutMs);
+		rightDrive->Config_kD(0, 0, Constants.kTimeoutMs);
+
 		// Collect Options
 		std::string targets = frc::DriverStation::GetInstance().GetGameSpecificMessage();
 		char optionTarget = targets[0];
